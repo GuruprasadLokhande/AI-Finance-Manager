00347bbbbe50a904324242cb2936b38c980499fa
GuruprasadLokhande <gurulokhande2915@gmail.com>
2025-02-12 15:00:41 +0530

    Header Component & User Authentication Done

---
+++ components/header.jsx
@@ -0,0 +1,63 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { LayoutDashboard, PenBox } from "lucide-react";
+
+const Header = () => {
+  return (
+    <div className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-5- border-b">
+      <nav className="container mx-auto py-4 px-4 flex items-center justify-between">
+        <Link href="/">
+          <Image
+            src={"/logo.png"}
+            alt="welth logo"
+            width={60}
+            height={200}
+            className="h-12 w-auto"
+          />
+        </Link>
+
+        <div className="flex items-center space-x-4">
+          <SignedIn>
+            <Link
+              href="/dashboard"
+              className="text-gray-600 hover:text-blue-200 flex gap-2 items-center"
+            >
+              <Button
+                variant="outline"
+                className="text-gray-600 hover:text-blue-200 flex gap-2 items-center"
+              >
+                <LayoutDashboard size={18} />
+                <span className="hidden md:inline">Dashboard</span>
+              </Button>
+            </Link>
+
+            <Link href="/transaction/create">
+              <Button className="flex gap-2 items-center">
+                <PenBox size={18} />
+                <span className="hidden md:inline">Add Transaction</span>
+              </Button>
+            </Link>
+          </SignedIn>
+          <SignedOut>
+            <SignInButton forceRedirectUrl="/dashboard">
+              <Button variant="outline">Login</Button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton
+              appearance={{
+                elements: {
+                  avatarBox: "w-10 h-10"
+                },
+              }}
+            />
+          </SignedIn>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
