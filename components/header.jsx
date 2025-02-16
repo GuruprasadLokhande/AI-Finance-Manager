637a413fbb5b3006b8da1301c8adead406566199
GuruprasadLokhande <gurulokhande2915@gmail.com>
2025-02-16 13:55:10 +0530

    Configuration of Database

--- components/header.jsx
+++ components/header.jsx
@@ -3,8 +3,10 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { LayoutDashboard, PenBox } from "lucide-react";
+import { checkUser } from "@/lib/checkuser";
 
-const Header = () => {
+const Header = async () => {
+  await checkUser();
   return (
     <div className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-[20] border-b">
       <nav className="container mx-auto py-4 px-4 flex items-center justify-between">
