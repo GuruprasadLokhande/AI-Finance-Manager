227f5295591325b8388ca0b92de709a60a4b2b63
GuruprasadLokhande <gurulokhande2915@gmail.com>
2025-02-22 11:17:30 +0530

    Set Monthly Budget Done

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -4,12 +4,27 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Plus } from "lucide-react";
 import React from "react";
 import AccountCard from "./_components/account-card";
+import { getCurrentBudget } from "@/actions/budget";
+import BudgetProgress from "./_components/budget-progress";
 
 async function DashboardPage() {
   const accounts = await getUserAccount();
+  const defaultAccount = accounts?.find((account) => account.isDefault);
+
+  // Get budget for default account
+  let budgetData = null;
+  if (defaultAccount) {
+    budgetData = await getCurrentBudget(defaultAccount.id);
+  }
+
+  console.log(budgetData);
   return (
-    <div className="px-5">
+    <div className="space-y-8">
       {/* Budget Progress  */}
+      <BudgetProgress
+        initialBudget={budgetData?.budget}
+        currentExpenses={budgetData?.currentExpenses || 0}
+      />
 
       {/* Overview */}
 
@@ -24,11 +39,10 @@ async function DashboardPage() {
           </Card>
         </CreateAccountDrawer>
 
-        {accounts.length > 0 && accounts.map((account) => {
-          return <AccountCard key={account.id} account={account}/>
-        }
-        
-        )}
+        {accounts.length > 0 &&
+          accounts.map((account) => {
+            return <AccountCard key={account.id} account={account} />;
+          })}
       </div>
     </div>
   );
